ea9a9afa4593936a18c4285a40713cf991ac5f0f
Nguyen Phuctan <nguyenphuctan.dev@gmail.com>
2023-02-02 11:04:07 +0700

    fix card info checking logic

--- src/main/java/ecobike/controllers/RentBikeController.java
+++ src/main/java/ecobike/controllers/RentBikeController.java
@@ -79,71 +79,75 @@ public class RentBikeController {
         if (text1.isEmpty() || text2.isEmpty() || text3.isEmpty()) {
             NotificationBox.display("NotificationBox", "Fill mandatory fields!");
         } else {
-            String command = "SELECT * FROM card";
-            ArrayList<ArrayList<String>> cards = MySQLDB.query(command);
-            boolean cardInfoMatched = false;
-            for (ArrayList<String> card : cards) {
-                if (card.get(0).equals(text1) && card.get(1).equals(text2) && card.get(2).equals(text3)) {
-                    cardInfoMatched = true;
-                    command = "SELECT * FROM event";
-                    ArrayList<ArrayList<String>> events = MySQLDB.query(command);
+            // call interbank API -> cardInfoMatched
+            boolean cardInfoMatched = true;
+            if (cardInfoMatched) {
+                String command = "SELECT * FROM card WHERE cardcode = " + text1;
+                ArrayList<ArrayList<String>> cards = MySQLDB.query(command);
+                if (cards.size() == 0) {
+                    String expireddate = "2025-01-13";
+                    command = "INSERT INTO card VALUES ('" + text1 + "', '" + text2 + "', '" + text3 + "', '" + expireddate + "')";
+                    MySQLDB.execute(command);
+                };
 
-                    Set<String> ongoingRentals = new HashSet<String> ();
-                    for (ArrayList<String> event : events) {
-                        if (event.get(3).equals("start")) {
-                            ongoingRentals.add(event.get(1));
-                        } else if (event.get(3).equals("end")) {
-                            ongoingRentals.remove(event.get(1));
-                        }
+                command = "SELECT * FROM event";
+                ArrayList<ArrayList<String>> events = MySQLDB.query(command);
+
+                Set<String> ongoingRentals = new HashSet<String> ();
+                for (ArrayList<String> event : events) {
+                    if (event.get(3).equals("start")) {
+                        ongoingRentals.add(event.get(1));
+                    } else if (event.get(3).equals("end")) {
+                        ongoingRentals.remove(event.get(1));
                     }
+                }
 
-                    command = "SELECT * FROM rental WHERE id in (";
-                    for (String ongoingRental : ongoingRentals) {
-                        command += "\"" + ongoingRental + "\"" + ", ";
+                command = "SELECT * FROM rental WHERE id in (";
+                for (String ongoingRental : ongoingRentals) {
+                    command += "\"" + ongoingRental + "\"" + ", ";
+                }
+                command = command.substring(0, command.length() - 2);
+                command += ")";
+                ArrayList<ArrayList<String>> ongoingRentalsDB = MySQLDB.query(command);
+
+                boolean cardCurrentlyOnRental = false;
+                for (ArrayList<String> ongoingRentalDB : ongoingRentalsDB) {
+                    if (ongoingRentalDB.get(3).equals(text1)) {
+                        cardCurrentlyOnRental = true;
                     }
-                    command = command.substring(0, command.length() - 2);
-                    command += ")";
-                    ArrayList<ArrayList<String>> ongoingRentalsDB = MySQLDB.query(command);
+                }
 
-                    boolean cardCurrentlyOnRental = false;
-                    for (ArrayList<String> ongoingRentalDB : ongoingRentalsDB) {
-                        if (ongoingRentalDB.get(3).equals(card.get(0))) {
-                            cardCurrentlyOnRental = true;
-                        }
-                    }
+                if (cardCurrentlyOnRental) {
+                    NotificationBox.display("NotificationBox", "Card currently on rental");
+                } else {
+                    boolean DepositConfirmation = ConfirmBox.display("ConfirmBox", "Proceed to deposit?");
+                    if (DepositConfirmation) {
+                        NotificationBox.display("NotificationBox", "Checking balance... Placing deposit");
+                        boolean depositResult = true;
+                        if (depositResult) {
+                            NotificationBox.display("NotificationBox", "Rent request successful!");
 
-                    if (cardCurrentlyOnRental) {
-                        NotificationBox.display("NotificationBox", "Card currently on rental");
-                    } else {
-                        boolean DepositConfirmation = ConfirmBox.display("ConfirmBox", "Proceed to deposit?");
-                        if (DepositConfirmation) {
-                            NotificationBox.display("NotificationBox", "Checking balance... Placing deposit");
-                            boolean depositResult = true;
-                            if (depositResult) {
-                                NotificationBox.display("NotificationBox", "Rent request successful!");
-
-                                command = "SELECT * FROM rental";
-                                ArrayList<ArrayList<String>> rentals = MySQLDB.query(command);
-                                String rentee_id = "1";
-                                command = "INSERT INTO rental VALUES (" + (rentals.size() + 1) + ", " + rentee_id + ", " + bikeID + ", '" + card.get(0) + "')";
-                                MySQLDB.execute(command);
-                                command = "INSERT INTO event VALUES (" + (events.size() + 1) + ", " + (rentals.size() + 1) + ", '" + LocalDateTime.now().format(DateTimeFormatter.ofPattern(PATTERN)) + "', 'start')";
-                                MySQLDB.execute(command);
-                                command = "SELECT * FROM paymenttransaction";
-                                ArrayList<ArrayList<String>> paymenttransactions = MySQLDB.query(command);
-                                command = "INSERT INTO paymenttransaction VALUES (" + (paymenttransactions.size() + 1) + ", " + (rentals.size() + 1) + ", " + text4 + ", '" + LocalDateTime.now().format(DateTimeFormatter.ofPattern(PATTERN)) + "', 'deposit')";
-                                MySQLDB.execute(command);
-                                command = "UPDATE bike SET is_rented = 1 WHERE id = " + bikeID + "";
-                                MySQLDB.execute(command);
-                            } else {
-                                NotificationBox.display("NotificationBox", "Not enough balance");
-                            }
+                            command = "SELECT * FROM rental";
+                            ArrayList<ArrayList<String>> rentals = MySQLDB.query(command);
+                            String rentee_id = "1";
+                            command = "INSERT INTO rental VALUES (" + (rentals.size() + 1) + ", " + rentee_id + ", " + bikeID + ", '" + text1 + "')";
+                            MySQLDB.execute(command);
+                            command = "INSERT INTO event VALUES (" + (events.size() + 1) + ", " + (rentals.size() + 1) + ", '" + LocalDateTime.now().format(DateTimeFormatter.ofPattern(PATTERN)) + "', 'start')";
+                            MySQLDB.execute(command);
+                            command = "SELECT * FROM paymenttransaction";
+                            ArrayList<ArrayList<String>> paymenttransactions = MySQLDB.query(command);
+                            command = "INSERT INTO paymenttransaction VALUES (" + (paymenttransactions.size() + 1) + ", " + (rentals.size() + 1) + ", " + text4 + ", '" + LocalDateTime.now().format(DateTimeFormatter.ofPattern(PATTERN)) + "', 'deposit')";
+                            MySQLDB.execute(command);
+                            command = "UPDATE bike SET is_rented = 1 WHERE id = " + bikeID + "";
+                            MySQLDB.execute(command);
+                        } else {
+                            NotificationBox.display("NotificationBox", "Not enough balance");
                         }
                     }
                 }
             }
-            if (!cardInfoMatched) {
-                NotificationBox.display("NotificationBox", "Card not found or CVV wrong!");
+            else {
+                NotificationBox.display("NotificationBox", "Wrong card info!");
             }
         }
     }
