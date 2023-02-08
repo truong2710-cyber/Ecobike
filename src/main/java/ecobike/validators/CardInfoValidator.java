eacbcbc429a946ed7daa8ad8af6fa44b7c63fcd6
truong2710-cyber <truong.vq194198@sis.hust.edu.vn>
2023-02-09 01:16:05 +0700

    add unit test

--- src/main/java/ecobike/validators/CardInfoValidator.java
+++ src/main/java/ecobike/validators/CardInfoValidator.java
@@ -2,19 +2,24 @@ package ecobike.validators;
 
 
 import java.time.LocalDate;
+import java.util.regex.Pattern;
 
 public class CardInfoValidator {
     public static boolean validateCardCode(String cardCode){
-        return true;
+        Pattern pattern = Pattern.compile("[0-9]{12}");
+
+        return pattern.matcher(cardCode).matches();
     }
     public static boolean validateOwner(String owner){
-        return true;
+        return (owner.length() > 0);
     }
     public static boolean validateCvv(String cvv){
-        return true;
+        return (cvv.length() > 0);
     }
     public static boolean validateExpireDate(LocalDate expireDate){
-        return true;
+        LocalDate now = LocalDate.now();
+
+        return expireDate.isAfter(now);
     }
 
 }
