dae5e14caf5bfb949d84be9db97b50289f73a66f
truong2710-cyber <truong.vq194198@sis.hust.edu.vn>
2023-02-09 01:57:21 +0700

    add unit test

--- src/test/java/validators/CardInfoValidatorTest.java
+++ src/test/java/validators/CardInfoValidatorTest.java
@@ -1,17 +1,23 @@
 package validators;
 
 import junitparams.FileParameters;
+import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import junitparams.JUnitParamsRunner;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 
 @RunWith(JUnitParamsRunner.class)
 public class CardInfoValidatorTest {
     @Test
-    @FileParameters("src/test/resources/cardCode.csv")
+    //@FileParameters("src/test/resources/cardCode.csv")
+    @Parameters({
+            "1|false",
+            "2|false",
+    })
     public void validateCardCode(String cardCode, String expected) {
         //boolean check = CardInfoValidator
         Assertions.assertEquals(1,1);
